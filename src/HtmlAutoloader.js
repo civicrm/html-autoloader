2a2b604bcd1d931a878456166cc1dc86807df9a7
Tim Otten <totten@civicrm.org>
2026-02-02 20:41:19 -0800

    Draft resource types

--- src/HtmlAutoloader.js
+++ src/HtmlAutoloader.js
@@ -6,6 +6,64 @@ class HtmlAutoloader {
     this.resourceTypes = {};
     this.loadedElements = new Set();
     this.loadedRules = new Set();
+
+    this.addResourceType({
+      name: 'js',
+      onLoad: (resource, elementName, elementRule) => {
+        return new Promise((resolve, reject) => {
+          const script = document.createElement('script');
+          script.src = resource;
+          script.onload = () => resolve(`JS loaded: ${resource}`);
+          script.onerror = () => reject(new Error(`Failed to load JS: ${resource}`));
+          document.head.appendChild(script);
+        });
+      }
+    });
+    this.addResourceType({
+      name: 'css',
+      onLoad: (resource, elementName, elementRule) => {
+        return new Promise((resolve, reject) => {
+          const link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.href = resource;
+          link.type = 'text/css';
+          link.onload = () => resolve(`CSS loaded: ${resource}`);
+          link.onerror = () => reject(new Error(`Failed to load CSS: ${resource}`));
+          document.head.appendChild(link);
+        });
+      }
+    });
+    this.addResourceType({
+      name: 'html',
+      onLoad: (resource, elementName, elementRule) => {
+        return fetch(resource)
+          .then(response => response.text())
+          .then(html => {
+            document.head.insertAdjacentHTML('beforeend', html);
+            return `HTML loaded: ${resource}`;
+          });
+      }
+    });
+    this.addResourceType({
+      name: 'module',
+      onLoad: (resource, elementName, elementRule) => {
+        return new Promise((resolve, reject) => {
+          const script = document.createElement('script');
+          script.type = 'module';
+          script.src = resource;
+          script.onload = () => resolve(`Module loaded: ${resource}`);
+          script.onerror = () => reject(new Error(`Failed to load module: ${resource}`));
+          document.head.appendChild(script);
+        });
+      }
+    });
+    this.addResourceType({
+      name: 'import',
+      onLoad: (resource, elementName, elementRule) => {
+        return import(resource)
+          .then(() => `Import loaded: ${resource}`);
+      }
+    });
   }
 
   addElement(elementRule) {
